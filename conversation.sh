85679399962c6fcb3bf2f70f4ca001decf952cf3
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-15 17:14:38 +0530

    modified conversation file

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Modi: Hey Trump, How are you?"
+echo "Trump: Hi Modi, I am good, How are you?"
+echo "Modi: I am fine too, what's up?"
+echo "Trump: Nice, Would you like to join me now?"
+echo "Modi: Nope, Thank you! I have got to leave on some urgent work"
+echo "end of the line"
