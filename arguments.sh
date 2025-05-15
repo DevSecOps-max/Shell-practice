d5f665223c99c1ffca68581701bc552930e84768
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-15 17:34:30 +0530

    added argument file

---
+++ arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON2: Hey $PERSON1, How are you?"
+echo "$PERSON1: Hi $PERSON2, I am good, How are you?"
+echo "$PERSON2: I am fine too, what's up?"
+echo "$PERSON1: Nice, Would you like to join me now?"
+echo "$PERSON2: Nope, Thank you! I have got to leave on some urgent work"
+echo "end of the line"
