c4acf14087ee1021744b564f37e157ec2cc9514d
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-16 12:39:21 +0530

    special vars

--- special_variables.sh
+++ special_variables.sh
@@ -2,3 +2,11 @@
 
 echo "All the variables passed to the script: $@"
 echo "Number of variables: $#"
+echo "script file name is: $0"
+echo "Present working directory is: $PWD"
+echo "user running the script: $USER"
+echo "home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last running command is: $!"
+
