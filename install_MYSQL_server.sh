f89386ed8db4b6a1c4b655ad68ad8ec7c9fcba70
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-16 15:28:27 +0530

    added additional lines to the MYSQL file

--- install_MYSQL_server.sh
+++ install_MYSQL_server.sh
@@ -11,15 +11,37 @@ else
     echo "running with the root access"
 fi
 
-dnf install mysql -y      #mysql installation command
+dnf list installed mysql
 
-if [ $? -eq 0 ]
+
+if [ $? -ne 0 ]
 then
-    echo "Installation of MYSQL is success......."
+    echo "MYSQL is not installed...will be installed shortly"
+    dnf install mysql -y
+        if [ $? -eq 0 ]
+    then
+        echo "Installation of MYSQL is success......."
+    else
+        echo "Installation of MYSQL is failed........"
+        exit 21
+    fi
 else
-    echo "Installation of MYSQL is failed........"
-    exit 21
+    echo "MYSQL is already installed.. Nothing to do"
+    exit 31
 fi
+
+
+
+
+# dnf install mysql -y      #mysql installation command
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installation of MYSQL is success......."
+# else
+#     echo "Installation of MYSQL is failed........"
+#     exit 21
+# fi
 
 
 
