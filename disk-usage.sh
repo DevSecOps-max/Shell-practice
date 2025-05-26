490a5458ee85d31dbeebae466569c880f108f532
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-26 17:39:10 +0530

    disk usage file

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,15 +2,21 @@
 
 DISK_USAGE=$( df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # Usually in realtime it will be above 70
+MSG=""
 
 
 while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7F}')
-    echo "$PARTITION: $USAGE"
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then
+        MSG="High disk usage observed on $PARTITION: $USAGE"
+    fi
     
 done <<< $DISK_USAGE
+
+echo $MSG
 
 
 
