2a27fcb33816d4a5d37a0af7b5a584d96279ecb8
DevSecOps-max <joindevopsladdu@gmail.com>
2025-05-26 17:34:40 +0530

    disk usage file

--- disk-usage.sh
+++ disk-usage.sh
@@ -7,7 +7,7 @@ DISK_THRESHOLD=1 # Usually in realtime it will be above 70
 while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $6F}')
+    PARTITION=$(echo $line | awk '{print $7F}')
     echo "$PARTITION: $USAGE"
     
 done <<< $DISK_USAGE
